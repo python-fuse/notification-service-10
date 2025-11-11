7eed31bb2905bd7a17bfbdbde636f2b2fa63488e
Elder User <eld3rusr@outlook.com>
2025-11-11 20:00:22 +0100

    feat: configure user service in Docker and update port settings

--- notification_services/user/src/main.ts
+++ notification_services/user/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
-  logger.log(`🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  await app.listen(process.env.PORT ?? 3001);
+  logger.log(`🚀 Application is running on: http://localhost:${process.env.PORT ?? 3001}`);
 }
 bootstrap();
