498fcc01bbfe21ea4048b53878d2eabbefc23040
Elder User <eld3rusr@outlook.com>
2025-11-12 09:50:09 +0100

    feat: add healthcheck to postgres_user service and enforce non-empty fields in CreateUserDto

--- notification_services/user/src/users/dto/create-user.dto.ts
+++ notification_services/user/src/users/dto/create-user.dto.ts
@@ -1,16 +1,19 @@
-import { IsEmail, IsString, MinLength, IsOptional, IsObject } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsOptional, IsObject, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
     @IsString()
+    @IsNotEmpty()
     first_name: string;
 
     @IsString()
+    @IsNotEmpty()
     last_name: string;
 
     @IsEmail()
+    @IsNotEmpty()
     email: string;
 
     @IsString()
-    @MinLength(8)
+    @MinLength(8, { message: 'Password must be at least 8 characters long' })
     password: string;
 
     @IsOptional()
